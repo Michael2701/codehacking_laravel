0a7009e05a116bb4c95d3f84e6e7e4dffc9753fb
Michael Silianov <michaelsilianov@gmail.com>
2016-06-18 08:06:05 +0300

    admin users upadate & create working | no security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -7,6 +7,7 @@ use App\Role;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEditRequest;
 
 use App\Http\Requests;
 
@@ -44,7 +45,16 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        $input = $request->all();
+        if(trim($request->password) == '')
+        {
+            $input = $request->except('password');
+        }
+        else
+        {
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+
 
         if($file = $request->file('photo_id'))
         {
@@ -57,8 +67,6 @@ class AdminUsersController extends Controller
             $input['photo_id'] = $photo->id;
 
         }
-
-        $input['password'] = bcrypt($request->password);
 
         User::create($input);
 
@@ -84,7 +92,10 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        return view('admin.users.edit');
+        $user = User::findOrFail($id);
+        $roles = Role::lists('name','id')->all();
+
+        return view('admin.users.edit', compact('user','roles'));
     }
 
     /**
@@ -94,9 +105,37 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        if(trim($request->password) == '')
+        {
+            $input = $request->except('password');
+        }
+        else
+        {
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+
+        if($file = $request->file('photo_id'))
+        {
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            //unlink( $user->photo->name,'images');
+
+            $photo = Photo::create(['name'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+
     }
 
     /**
---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts/admin')
+
+@section('content')
+    <h1 class="text-center">Edit User</h1>
+<div class="col-md-3">
+    <img src="{{$user->photo ? $user->photo->name : '/images/user.png'}}" alt="" class="img-responsive">
+</div>
+<div class="col-md-9">
+    {!! Form::model($user, ['method'=>'PATCH', 'action'=>['AdminUsersController@update', $user->id], 'files'=>true]) !!}
+
+    <div class="form-group">
+
+        {!! Form::label('name', 'Name:') !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('email', 'Email:') !!}
+        {!! Form::email('email', null, ['class' => 'form-control']) !!}
+
+    </div>
+
+
+    <div class="form-group">
+
+        {!! Form::label('role_id', 'Role:') !!}
+        {!! Form::select('role_id',$roles, null, ['class' => 'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('is_active', 'Status:') !!}
+        {!! Form::select('is_active', array(1=>'Active',0=>'Not Active'),null, ['class' => 'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('photo_id', 'Image:') !!}
+        {!! Form::file('photo_id', ['class' => 'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('password', 'Password:') !!}
+        {!! Form::password('password', ['class' => 'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+        {!! Form::submit('Update User', ['class'=>'btn btn-info']) !!}
+    </div>
+
+    {!! Form::close() !!}
+
+
+    @include('includes.form_errors')
+</div>
+
+@endsection
+
+
+
+@section('footer')
+@endsection
